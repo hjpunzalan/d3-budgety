380898318d410c1830bc46c4653bd1eae708f528
hjpunzalan <hj.punzalan@hotmail.com>
2019-10-23 23:01:00 +0800

    drew the enter selection in typescript

--- src/graph.ts
+++ src/graph.ts
@@ -1,6 +1,9 @@
 import * as d3 from 'd3';
 
+declare const db: any;
+
 interface pieData {
+	id: string;
 	name: string;
 	cost: number;
 }
@@ -24,11 +27,44 @@ export default (function() {
 		.sort(null) // prevents default sorting
 		.value(d => d.cost);
 
-	const angles = pie([
-		{ name: 'rent', cost: 500 },
-		{ name: 'bills', cost: 300 },
-		{ name: 'gaming', cost: 200 }
-	]);
+	const arcPath = d3
+		.arc<d3.PieArcDatum<pieData>>()
+		.outerRadius(dims.radius)
+		.innerRadius(dims.radius / 2);
 
-	console.log(angles);
+	// update function
+	const update = (data: pieData[]): void => {
+		// join enhanced (pie) data to path element
+		const paths = graph.selectAll('path').data(pie(data));
+
+		paths
+			.enter()
+			.append('path')
+			.attr('class', 'arc')
+			.attr('d', (d: d3.PieArcDatum<pieData>) => arcPath(d))
+			.attr('stroke', '#fff')
+			.attr('stroke-width', 3);
+	};
+
+	let data: pieData[] = [];
+	db.collection('expenses').onSnapshot((res: any) => {
+		res.docChanges().forEach((change: any) => {
+			const doc = { ...change.doc.data(), id: change.doc.id };
+			switch (change.type) {
+				case 'added':
+					data.push(doc);
+					break;
+				case 'modified':
+					const index = data.findIndex(item => item.id === doc.id);
+					data[index] = doc;
+					break;
+				case 'removed':
+					data = data.filter(item => item.id !== doc.id);
+					break;
+				default:
+					break;
+			}
+		});
+		update(data);
+	});
 })();
